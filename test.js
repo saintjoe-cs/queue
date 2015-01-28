d5f97886778a7e0c84c9c9091ab9039dc60107a2
Brian Capouch <brianc@saintjoe.edu>
2015-01-28 00:47:21 -0500

    Example 5-1 code from McMillan

---
+++ test.js
@@ -0,0 +1,11 @@
+load('queue.js');
+// test program
+var q = new Queue();
+q.enqueue("Meredith");
+q.enqueue("Cynthia");
+q.enqueue("Jennifer");
+print(q.toString());
+q.dequeue();
+print(q.toString());
+print("Front of queue: " + q.front());
+print("Back of queue: " + q.back());
