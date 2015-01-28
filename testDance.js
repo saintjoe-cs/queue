3ce58c0570b66ab4b0257815a6aa5989f0636bb6
Brian Capouch <brianc@saintjoe.edu>
2015-01-28 01:21:37 -0500

    Test programs for square dance matcher

---
+++ testDance.js
@@ -0,0 +1,14 @@
+// test program
+load('queue.js');
+load('dance.js');
+var maleDancers = new Queue();
+var femaleDancers = new Queue();
+getDancers(maleDancers, femaleDancers);
+dance(maleDancers, femaleDancers);
+if (!femaleDancers.empty()) {
+ print(femaleDancers.front().name + " is waiting to dance.");
+}
+if (!maleDancers.empty()) {
+ print(maleDancers.front().name + " is waiting to dance.");
+}
+
