3dabd15c4df6b9deaf60f1e90e5c26cb9b254acb
Brian Capouch <brianc@proto.palaver.net>
2015-05-26 01:00:30 -0400

    Fix formatting

--- ex5-3.js
+++ ex5-3.js
@@ -1,5 +1,5 @@
-load('queue.js');
-load('dance.js');
+load('Queue.js');
+load('Dancer.js');
 
 var maleDancers = new Queue();
 var femaleDancers = new Queue();
@@ -7,11 +7,11 @@ getDancers(maleDancers, femaleDancers);
 print('There are ' + femaleDancers.count() + ' females and ' + maleDancers.count() + ' males waiting to dance.');
 dance(maleDancers, femaleDancers);
 if (maleDancers.count() > 0) {
- print("There are " + maleDancers.count() +
- " male dancers waiting to dance.");
+  print("There are " + maleDancers.count() +
+   " male dancers waiting to dance.");
 }
 if (femaleDancers.count() > 0) {
- print("There are " + femaleDancers.count() +
- " female dancers waiting to dance.");
+  print("There are " + femaleDancers.count() +
+   " female dancers waiting to dance.");
 }
 
